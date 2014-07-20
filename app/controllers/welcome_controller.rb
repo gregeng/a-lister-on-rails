9d20e16732424bb4e487733af0f47426ef233ba0
Greg Eng <greg.eng@gmail.com>
2014-07-20 16:36:53 -0700

    revert to using rotten tomatoes for image data since imdb blocks direct linking from third party sites http://stackoverflow.com/questions/11044010/imdb-poster-url-returns-referral-denied

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,22 +5,20 @@ class WelcomeController < ApplicationController
 
   def show
     if params[:actor]
-      # @actor_image = ActorImageScraper.scrape_actor_image(params[:actor])
-
       actor_name = params[:actor]
-      actor_imdb_id = FreebaseApi.get_actor_details(actor_name)
+      # actor_imdb_id = FreebaseApi.get_actor_details(actor_name)
+      # @actor_image = ImdbActorImageScraper.scrape_actor_image(actor_imdb_id)
 
-      @actor_image = ImdbActorImageScraper.scrape_actor_image(actor_imdb_id)
+      @actor_image = ActorImageScraper.scrape_actor_image(actor_name)
       @actor = actor_name
       @movies = FreebaseApi.get_actor_movies(actor_name)
     else
-      # rt_movie_object = RottenTomatoesApi.search(params[:movie])
-      # @movie_poster_image = rt_movie_object["movies"].first["posters"]["original"] if rt_movie_object["movies"]
+      rt_movie_object = RottenTomatoesApi.search(params[:movie])
+      # movie_imdb_id = FreebaseApi.get_movie_thumbnail(movie_title)
+      # @movie_poster_image = MoviePosterScraper.scrape_movie_poster(movie_imdb_id)
 
+      @movie_poster_image = rt_movie_object["movies"].first["posters"]["original"].gsub("tmb", "ori") if rt_movie_object["movies"]
       movie_title = params[:movie]
-      movie_imdb_id = FreebaseApi.get_movie_thumbnail(movie_title)
-
-      @movie_poster_image = MoviePosterScraper.scrape_movie_poster(movie_imdb_id)
       @movie = movie_title
       @actors = FreebaseApi.get_movie_cast(movie_title)
     end
