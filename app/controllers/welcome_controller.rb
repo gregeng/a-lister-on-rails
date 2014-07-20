43e2ecc55a08c86e852135b874fe393a7e0bafdc
Greg Eng <greg.eng@gmail.com>
2014-07-20 15:12:48 -0700

    get some proof of concept things written to add ability to search for a movie's actors or an actor's movies

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,17 @@
 class WelcomeController < ApplicationController
+
+  def index
+  end
+
+  def show
+    if params[:actor]
+      @actor = params[:actor]
+      @movies = FreebaseApi.get_actor_movies(params[:actor])
+    else
+      @movie = params[:movie]
+      @actors = FreebaseApi.get_movie_cast(params[:movie])
+    end
+  end
+
+
 end
