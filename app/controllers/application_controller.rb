1c701759d3442501d50c625619e0ef6e1493fc65
Greg Eng <greg.eng@gmail.com>
2014-07-26 16:55:58 -0700

    add a current game method to application controller. start building out game_sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_game
+    if cookies[:game_id]
+      game_id = cookies.signed[:game_id]
+      @current_game = Game.find_by_id(game_id)
+    end
+  end
+
+  helper_method :current_game
+
 end
