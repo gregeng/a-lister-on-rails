0da9e52438a51c638718b52e10abb6df623250ad
Greg Eng <greg.eng@gmail.com>
2014-07-26 17:31:34 -0700

    add ability to create a new game sessions and game

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -4,12 +4,20 @@ class GameSessionsController < ApplicationController
   end
 
   def create
-    game = Game.create(params[:game_session])
-    cookies[:game_id] = game.id
+    game = Game.create(game_session_params)
+    cookies.signed[:game_id] = game.id
     redirect_to welcome_path
   end
 
   def destroy
+    cookies.delete(:game_id)
+    redirect_to welcome_path
+  end
+
+  private
+
+  def game_session_params
+    params.require(:game_session).permit(:starting_actor)
   end
 
 end
